411984c144a12d09b2ebf7f90616884005089dff
Harshit Raj <harshitr20@iitk.ac.in>
2021-11-08 17:13:39 +0530

    Cleanup

--- Assingment3/Assingment3.cpp
+++ Assingment3/Assingment3.cpp
@@ -1,7 +1,7 @@
 // File:   Bipartition.cpp
 // Date: Nov 8, 2021
 // Author: Harshit Raj (harshitr20@iitk.ac.in)
-// Author: Shubhan R (shubhanr20@iitk.ac.in) 
+// Author: Shubhan R (shubhanr20@iitk.ac.in)
 
 #include<iostream>
 using namespace std;
@@ -12,7 +12,7 @@ public:
 	// Structure of each node
 	typedef struct Node
 	{
-		int adj;
+		int adj;	// Adjacent node index
 		struct Node *next;
 		struct Node *prev;
 	} node;
@@ -65,9 +65,10 @@ class Graph
 {
 private:
 	AdjacencyList *arr;
+	// N: number of nodes, E: number of edges
 	int N, E;
 
-	// Function adds the edge from u to v
+	// Function adds the edge from v to w and w to v
 	void addEdge(int v, int w)
 	{
 		arr[v].add(w);
@@ -86,19 +87,27 @@ public:
 	// bipartition utility function
 	bool bipartitionUtil(int v, int set[])
 	{
+		// flag to check if the graph is bipartite or not
 		bool ok = true;
+
+		// ajacent nodes must of alternate set
 		int altset = set[v] == 1 ? 2 : 1;
 
+		// iterating over the adjacency list of v
 		AdjacencyList::node *temp = arr[v].nil->next;
-		while (temp != arr[v].nil)
+		while (temp != arr[v].nil && ok)
 		{
+			// if the adjacent node is in no set
 			if (set[temp->adj] == -1)
 			{
 				set[temp->adj] = altset;
 				ok = ok && bipartitionUtil(temp->adj, set);
 			}
-			else if (set[temp->adj] == set[v])
+
+			// if the adjacent node is in the same set
+			if (set[temp->adj] == set[v])
 				return false;
+
 			temp = temp->next;
 		}
 
@@ -108,15 +117,19 @@ public:
 	// bipartiton the graph
 	void bipartition()
 	{
+		// set to store the set of each node
 		int set[N];
 		for (int i = 0; i < N; i++)
 			set[i] = -1;
 
+		//  looping over the nodes
 		for (int i = 0; i < N; i++)
 		{
+			// if the node is not in any set
 			if (set[i] == -1)
 			{
-				set[i] = 1;
+				// set the node to set 1
+				set[i] = 1;		
 				if (!bipartitionUtil(i, set))
 				{
 					cout << "NO" << endl;
@@ -125,6 +138,8 @@ public:
 			}
 		}
 
+		// the graph is bipartite
+		// printing the set of each node
 		cout << "YES" << endl;
 		for (int i=0; i<N; i++)
 			cout << set[i] << endl;
@@ -133,16 +148,20 @@ public:
 	// take input from stdin
 	void input()
 	{
+		//  looping over the edges
 		for (int i = 0; i < E; i++)
 		{
 			int v, w;
 			cin >> v >> w;
 
+			// check for invalid vertex index
 			if (v < 0 || v >= N || w < 0 || w >= N)
 			{
 				cerr << "Invalid vertex" << endl;
 				return;
 			}
+
+			// add the edge
 			addEdge(v, w);
 		}
 	}
